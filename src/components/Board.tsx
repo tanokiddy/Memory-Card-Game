593ba019a8870f4f7751c81564b7a03f864252f1
Quang <quang@Killer-b-MBA.local>
2023-10-04 16:32:59 +0700

    feat: render initial Grid

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,7 +2,22 @@ import React, { useState, useEffect } from 'react';
 import Cell from './Cell';
 import './Board.css';
 
+const Shape = [
+  'Circle', "Square", "Triangle"
+]
+
+const Color = [
+  'Red', 'Green', "Blue"
+]
+
 const Board: React.FC = () => {
+  const Grid = Array.from({length: 16}, (_, i) => {
+    const randomNumber = Math.floor(Math.random() * (3 - 1 + 1) + 1) - 1
+    return {
+      shape: Shape[randomNumber],
+      color: Color[randomNumber]
+    }
+  })
   // states...
   useEffect(() => {
     // Initialize the game board with random shapes and colors
@@ -15,6 +30,11 @@ const Board: React.FC = () => {
   return (
     <div className="board">
       {/* Render each cell in the board */}
+      {Grid.map(item => (
+        <div className={`board-item item-${item.color} item-${item.shape}` } key={item.color}>
+          {item.color}
+        </div>
+      ))}
     </div>
   );
 };
